75a9b8912749655d057f487fcbcab79d46ca1b6a
Achebe Chika Luke <prosperbinary@gmail.com>
2022-11-20 16:00:44 -0800

    Education is when you read the fine print

--- 0x0_converter.c
+++ 0x0_converter.c
@@ -21,14 +21,14 @@ char *convert(unsigned long int num, int base, int lowercase)
 	rep = (lowercase)
 		? "0123456789abcdef"
 		: "0123456789ABCDEF";
-		ptr = &buffer[49];
-		*ptr = '\0';
-		do {
-			*--ptr = rep[num % base];
-			num /= base;
-		} while (num != 0);
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
 
-		return (ptr);
+	return (ptr);
 }
 
 
